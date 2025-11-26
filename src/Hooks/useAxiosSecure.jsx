a367d42b8b628002b0342e7e21b75e34597069f2
Mehad Hossen <mehad004938@gmail.com>
2025-11-26 14:12:18 +0600

    implement interceptor

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
 import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
 
 
@@ -9,12 +10,39 @@ const instance = axios.create({
 });
 
 const useAxiosSecure = () => {
-  const { user } = useAuth();
+  const navigate=useNavigate()
+  const { user, userLogout } = useAuth();
   useEffect(()=>{
-    instance.interceptors.request.use(config=>{
+    //interceptor request 
+
+    const requestInterceptor=instance.interceptors.request.use(config=>{
       config.headers.Authorization=`Bearer ${user?.accessToken}`
       return config;
     })
+    //  interceptor response
+    const responseInterceptor=instance.interceptors.response.use((response)=>{
+
+      return response;
+    },(error)=>{
+      console.log(error)
+
+      const errorStatus = error.status;
+      if(errorStatus===401|| errorStatus===403){
+         userLogout().then(()=>{
+          navigate("/login")
+
+         })
+
+      }
+      return Promise.reject(error)
+    })
+
+return ()=>{
+  instance.interceptors.request.eject(requestInterceptor)
+  instance.interceptors.response.eject(responseInterceptor)
+
+}
+
   })
     return instance
 };
